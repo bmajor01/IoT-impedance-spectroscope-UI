003e0ff9f02f32b280ae642d935700601a5ccf50
Bela Major <36770843+bmajor01@users.noreply.github.com>
2020-05-03 09:47:00 +0200

    init

---
+++ Impedance Spectroscope/Form1.Designer.vb
@@ -0,0 +1,491 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Connect = New System.Windows.Forms.Button()
+        Me.StartFreq = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM7"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.Control
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Button1.Location = New System.Drawing.Point(12, 80)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(37, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "CH1"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 109)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(37, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "CH2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 138)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(37, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "CH3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 167)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(37, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "CH4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 196)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(37, 23)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "CH5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(12, 225)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(37, 23)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "CH6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 10)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(75, 20)
+        Me.TextBox1.TabIndex = 6
+        Me.TextBox1.Text = "COM7"
+        '
+        'Connect
+        '
+        Me.Connect.Location = New System.Drawing.Point(12, 36)
+        Me.Connect.Name = "Connect"
+        Me.Connect.Size = New System.Drawing.Size(75, 23)
+        Me.Connect.TabIndex = 7
+        Me.Connect.Text = "Connect"
+        Me.Connect.UseVisualStyleBackColor = True
+        '
+        'StartFreq
+        '
+        Me.StartFreq.Location = New System.Drawing.Point(12, 281)
+        Me.StartFreq.Name = "StartFreq"
+        Me.StartFreq.Size = New System.Drawing.Size(137, 23)
+        Me.StartFreq.TabIndex = 8
+        Me.StartFreq.Text = "Start Frequency"
+        Me.StartFreq.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(12, 310)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(137, 23)
+        Me.Button8.TabIndex = 9
+        Me.Button8.Text = "Number of increments"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(12, 339)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(137, 23)
+        Me.Button9.TabIndex = 10
+        Me.Button9.Text = "Increment"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(12, 368)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(137, 23)
+        Me.Button10.TabIndex = 11
+        Me.Button10.Text = "Settling cycle"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(155, 281)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(51, 20)
+        Me.TextBox3.TabIndex = 13
+        Me.TextBox3.TabStop = False
+        Me.TextBox3.Text = "1000"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(155, 310)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(51, 20)
+        Me.TextBox4.TabIndex = 14
+        Me.TextBox4.Text = "10"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(155, 339)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(51, 20)
+        Me.TextBox5.TabIndex = 15
+        Me.TextBox5.Text = "100"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(155, 368)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(51, 20)
+        Me.TextBox6.TabIndex = 16
+        Me.TextBox6.Text = "10"
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(12, 397)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(98, 81)
+        Me.Button11.TabIndex = 17
+        Me.Button11.Text = "Measure"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Chart1
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
+        Me.Chart1.Location = New System.Drawing.Point(212, 42)
+        Me.Chart1.Name = "Chart1"
+        Series1.ChartArea = "ChartArea1"
+        Series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line
+        Series1.Legend = "Legend1"
+        Series1.Name = "Impedancia"
+        Me.Chart1.Series.Add(Series1)
+        Me.Chart1.Size = New System.Drawing.Size(878, 593)
+        Me.Chart1.TabIndex = 18
+        Me.Chart1.Text = "Chart1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(209, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 19
+        Me.Label1.Text = "Device IDN"
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(74, 138)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(75, 23)
+        Me.Button12.TabIndex = 20
+        Me.Button12.Text = "Gain 5X"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(74, 167)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(75, 23)
+        Me.Button13.TabIndex = 21
+        Me.Button13.Text = "Rfb = 100R"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(74, 196)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(75, 23)
+        Me.Button14.TabIndex = 22
+        Me.Button14.Text = "Rfb = 1k"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(74, 225)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(75, 23)
+        Me.Button15.TabIndex = 23
+        Me.Button15.Text = "Rfb = 10k"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(209, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 13)
+        Me.Label2.TabIndex = 24
+        Me.Label2.Text = "Device STATE"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(139, 26)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(67, 13)
+        Me.Label3.TabIndex = 25
+        Me.Label3.Text = "Device resp:"
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(24, 507)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(98, 81)
+        Me.Button7.TabIndex = 26
+        Me.Button7.Text = "Measure"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(484, 26)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 13)
+        Me.Label4.TabIndex = 27
+        Me.Label4.Text = "Device state:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(548, 26)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(67, 13)
+        Me.Label5.TabIndex = 28
+        Me.Label5.Text = "Device resp:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(107, 109)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(67, 13)
+        Me.Label6.TabIndex = 29
+        Me.Label6.Text = "Device resp:"
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(110, 80)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(75, 23)
+        Me.Button16.TabIndex = 30
+        Me.Button16.Text = "Button16"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(220, 50)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 31
+        Me.Label7.Text = "Label7"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(810, 9)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(32, 13)
+        Me.Label8.TabIndex = 32
+        Me.Label8.Text = "Real:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(810, 26)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(33, 13)
+        Me.Label9.TabIndex = 33
+        Me.Label9.Text = "Imag:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(871, 9)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(45, 13)
+        Me.Label10.TabIndex = 34
+        Me.Label10.Text = "Label10"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(871, 26)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(45, 13)
+        Me.Label11.TabIndex = 35
+        Me.Label11.Text = "Label11"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1102, 647)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Chart1)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.StartFreq)
+        Me.Controls.Add(Me.Connect)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Impedance spectroscope"
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Connect As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents StartFreq As Button
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Impedance Spectroscope/Form1.vb
@@ -0,0 +1,286 @@
+﻿Imports System.Math
+Imports System.IO.Ports
+
+Public Class Form1
+    Dim measurement As Boolean
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SendCommand(Commands.CMD_SET_CH1)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SendCommand(Commands.CMD_SET_CH2)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        SendCommand(Commands.CMD_SET_CH3)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        SendCommand(Commands.CMD_SET_CH4)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        SendCommand(Commands.CMD_SET_CH5)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        SendCommand(Commands.CMD_SET_CH6)
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Function RefreshState() As String
+
+        Dim stateString As String
+
+        If SerialPort1.IsOpen Then
+            Dim cmd2() As Byte = {120, 13}
+            SerialPort1.Write(cmd2, 0, 2)
+            stateString = SerialPort1.ReadLine()
+            Label5.Text = stateString
+
+        End If
+
+        If (stateString(1) = "1") Then
+            Button1.BackColor = Color.Green
+        Else
+            Button1.BackColor = Color.White
+        End If
+
+        If (stateString(2) = "1") Then
+            Button2.BackColor = Color.Green
+        Else
+            Button2.BackColor = Color.White
+        End If
+
+        If (stateString(3) = "1") Then
+            Button3.BackColor = Color.Green
+        Else
+            Button3.BackColor = Color.White
+        End If
+
+        If (stateString(4) = "1") Then
+            Button4.BackColor = Color.Green
+        Else
+            Button4.BackColor = Color.White
+        End If
+
+        If (stateString(5) = "1") Then
+            Button5.BackColor = Color.Green
+        Else
+            Button5.BackColor = Color.White
+        End If
+
+        If (stateString(6) = "1") Then
+            Button6.BackColor = Color.Green
+        Else
+            Button6.BackColor = Color.White
+        End If
+
+        If (stateString(7) = "1") Then
+            Button13.BackColor = Color.Green
+        Else
+            Button13.BackColor = Color.White
+        End If
+
+        If (stateString(8) = "1") Then
+            Button14.BackColor = Color.Green
+        Else
+            Button14.BackColor = Color.White
+        End If
+
+        If (stateString(9) = "1") Then
+            Button15.BackColor = Color.Green
+        Else
+            Button15.BackColor = Color.White
+
+        End If
+        If (stateString(10) = "1") Then
+            Button12.BackColor = Color.Green
+        Else
+            Button12.BackColor = Color.White
+        End If
+
+        If (stateString(0) = "1") Then
+            Button11.BackColor = Color.Red
+            Button11.Text = "Measurement running"
+        Else
+            Button11.BackColor = Color.Green
+            Button11.Text = "Measurement stopped"
+        End If
+
+
+        Return stateString
+
+    End Function
+
+    Private Sub Connect_Click(sender As Object, e As EventArgs) Handles Connect.Click
+
+        SerialPort1.Close()
+        SerialPort1.PortName = TextBox1.Text
+        ' AddHandler SerialPort1.DataReceived, AddressOf DataReceivedHandler
+        SerialPort1.Open()
+
+        If SerialPort1.IsOpen Then
+            ErrorProvider1.Clear()
+            Dim cmd() As Byte = {97, 13}
+            SerialPort1.Write(cmd, 0, 2)
+            Dim v As String
+            v = SerialPort1.ReadLine()
+            Label1.Text = v
+        End If
+
+        RefreshState()
+
+    End Sub
+
+    Private Sub StartFreq_Click(sender As Object, e As EventArgs) Handles StartFreq.Click
+        SendValue(Commands.CMD_RECIEVE_STARTFREQ, TextBox3.Text)
+
+    End Sub
+
+    Private Sub TextBox6_TextChanged(sender As Object, e As EventArgs) Handles TextBox6.TextChanged
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        'SendCommand(Commands.CMD_START_MEAS)
+        measurement = True
+        If SerialPort1.IsOpen Then
+            Dim cmd() As Byte = {Commands.CMD_START_MEAS, 13}
+            SerialPort1.Write(cmd, 0, 2)
+            Label2.Text = SerialPort1.ReadLine()
+        Else
+            ErrorProvider1.SetError(Button6, "Serial port not open!")
+
+        End If
+
+        'GetData()
+
+    End Sub
+
+    Function GetData()
+
+        Dim data As String
+
+        'While (data <> "CMD_STOP_MEAS")
+        Dim cmd() As Byte = {Commands.CMD_DO_SWEEP, 13}
+        SerialPort1.Write(cmd, 0, 2)
+
+        data = SerialPort1.ReadLine()
+
+            Dim fields() As String = Split(data, ",")
+            Dim real As Integer = CInt(fields(1))
+            Dim imag As Integer = CInt(fields(3))
+
+        Label10.Text = real
+        Label11.Text = imag
+
+        Me.Chart1.Series("Impedancia").Points.AddXY(real, imag)
+        'End While
+
+        'Dim data As String
+        'data = SerialPort1.ReadLine()
+
+    End Function
+
+    Private Sub Chart1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+
+        For index As Integer = 0 To 3140 * 2
+
+        Next
+
+    End Sub
+    Enum Commands As Byte
+        CMD_GETSTATE = 120
+        CMD_IDN = 97
+        CMD_START_MEAS = 101
+        CMD_STOP_MEAS = 100
+        CMD_SET_CH1 = 49
+        CMD_SET_CH2 = 50
+        CMD_SET_CH3 = 51
+        CMD_SET_CH4 = 52
+        CMD_SET_CH5 = 53
+        CMD_SET_CH6 = 54
+        CMD_SET_RFB_100R = 55
+        CMD_SET_RFB_1k = 56
+        CMD_SET_RFB_10k = 57
+        CMD_SET_Rin = 48
+        CMD_SET_GAIN = 103
+        CMD_RECIEVE_STARTFREQ = 105
+        CMD_RECIEVE_INCREMENTS = 106
+        CMD_RECIEVE_NUMBERINCREMENTS = 107
+        CMD_RECIEVE_SETTLING = 108
+        CMD_DO_SWEEP = 104
+    End Enum
+    Function SendCommand(command As Commands)
+        If SerialPort1.IsOpen Then
+            Dim cmd() As Byte = {command, 13}
+            SerialPort1.Write(cmd, 0, 2)
+            Label2.Text = SerialPort1.ReadLine()
+        Else
+            ErrorProvider1.SetError(Button6, "Serial port not open!")
+            Return -1
+        End If
+        RefreshState()
+        Return 0
+    End Function
+
+    Function SendValue(command As Commands, value As Integer)
+
+        Dim cmd() As Byte = {command}
+        SerialPort1.Write(cmd, 0, 1)
+        SerialPort1.WriteLine(value)
+        Dim newline() As Byte = {13}
+        SerialPort1.Write(newline, 0, 1)
+        Label2.Text = SerialPort1.ReadLine()
+        RefreshState()
+        Return 0
+    End Function
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        SendCommand(Commands.CMD_SET_GAIN)
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        SendCommand(Commands.CMD_SET_RFB_100R)
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        SendCommand(Commands.CMD_SET_RFB_1k)
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        SendCommand(Commands.CMD_SET_RFB_10k)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        SendValue(Commands.CMD_RECIEVE_NUMBERINCREMENTS, TextBox4.Text)
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        SendValue(Commands.CMD_RECIEVE_INCREMENTS, TextBox5.Text)
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        SendValue(Commands.CMD_RECIEVE_SETTLING, TextBox6.Text)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim state As String = RefreshState()
+        If (state(0) = "1") Then
+            GetData()
+        End If
+    End Sub
+End Class
